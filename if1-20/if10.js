e7fd979d66e219032f671fff086419af59e528ae
SunnatAxmatovWebdev <zoiryakubov1958@gmail.com>
2025-01-15 15:08:32 +0500

    If changed

--- if1-20/if10.js
+++ if1-20/if10.js
@@ -1,1 +1,12 @@
-let a = -7;
+let a = 10;
+if(a > 0 && a%2!=0){
+    console.log("Musbat toq son")
+} else if(a > 0 && a%2==0){
+    console.log("Musbat juft son");
+} else if(a < 0 && a%2!=0){
+    console.log("Manfiy toq son");
+} else if(a < 0 && a%2==0){
+    console.log("Manfiy juft son");
+} else{
+    console.log("son 0 ga teng");
+}
---
+++ if1-20/if11.js
@@ -0,0 +1,5 @@
+let a = 30;
+let s;
+if((a > 0 && a % 2 === 0) && (a >=10)){
+  s = " Ikki xonali juft son";
+} else if()
