8ed50c099a927d7966cd3dac6166aa1581b4401c
SunnatAxmatovWebdev <zoiryakubov1958@gmail.com>
2025-01-27 20:41:28 +0500

    for added

---
+++ for/for3.js
@@ -0,0 +1,15 @@
+let a = 8;
+
+let b = 11;
+
+let numbers = [];
+if (a > b) {
+  console.log("a son b sondan kichik bo'lishi");
+} else {
+  for (let i = a+1; i < b; i++) {
+    numbers.push(i);
+        console.log(i);
+        
+  }
+}
+   
